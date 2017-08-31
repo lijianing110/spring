c6ebf2037b7d3628a91b259c042b6ca2d43c5b85
lijianing <lijianing110@126.com>
2017-08-31 18:54:36 +0800

    none

--- spring-base-test/src/main/java/com/lijianing/spring/test/TestAutoWired.java
+++ spring-base-test/src/main/java/com/lijianing/spring/test/TestAutoWired.java
@@ -1,6 +1,7 @@
 package com.lijianing.spring.test;
 
 import com.lijianing.spring.Bean2;
+import com.lijianing.spring.Bean4;
 import com.lijianing.spring.config.SpringConfigAutoWired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -13,5 +14,7 @@ public class TestAutoWired {
         ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfigAutoWired.class);
         Bean2 bean2 = context.getBean(Bean2.class);
         bean2.execute();
+        Bean4 bean4 = context.getBean(Bean4.class);
+        bean4.execute();
     }
 }
